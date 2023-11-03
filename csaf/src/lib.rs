421da997562ea6075ce9dec7fe79c7d360a3b1c0
Jens Reimann <ctron@dentrassi.de>
2023-11-03 08:49:20 +0100

    docs: add some more

--- csaf/src/lib.rs
+++ csaf/src/lib.rs
@@ -1,4 +1,20 @@
-//! Walking through CSAF advisories
+//! Walking through CSAF documents
+//!
+//! ## Idea
+//!
+//! The basic idea is to provide a mechanism to walk over documents from differences sources
+//! ([`csaf_walker::source::HttpSource`] or [`csaf_walker::source::FileSource`]). Then
+//! chaining visitors in a layered fashion depending on your use case, extending the information
+//! known about a CSAF document. That doesn't mean to actually parse the document, but the ensure
+//! things like integrity, by digests and signatures.
+//!
+//! The stack allows one to customize the walking process, like skipping existing documents, or
+//! processing only changed documents.
+//!
+//! The last step, most likely, is to do something with a discovered document (like storing,
+//! uploading, evaluating). This is up to user to implement this. However, for some common use
+//! cases, the [`csaf_cli`](https://crates.io/crates/csaf-cli) crate might have some
+//! out-of-the-box tooling for the command line.
 //!
 //! ## Example
 //!
--- csaf/src/model/mod.rs
+++ csaf/src/model/mod.rs
@@ -1,1 +1,2 @@
+//! Data models
 pub mod metadata;
