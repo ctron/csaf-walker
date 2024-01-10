ca1a9abf30c0709f0db2a6b5215b96504f598c47
Jens Reimann <ctron@dentrassi.de>
2024-01-10 13:36:13 +0100

    chore: only expose if we have any use of it

--- extras/src/visitors/mod.rs
+++ extras/src/visitors/mod.rs
@@ -1,5 +1,7 @@
 //! Additional out-of-the-box visitors
 
+#[cfg(any(feature = "csaf-walker", feature = "sbom-walker"))]
 mod ignore;
 
+#[cfg(any(feature = "csaf-walker", feature = "sbom-walker"))]
 pub use ignore::*;
