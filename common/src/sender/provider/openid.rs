e75fa144bdb1c94828d139f2da80532f6ea84c33
Jens Reimann <ctron@dentrassi.de>
2024-01-12 09:52:37 +0100

    feat: allow using insecure TLS with OIDC issuer

--- common/src/sender/provider/openid.rs
+++ common/src/sender/provider/openid.rs
@@ -36,6 +36,13 @@ pub struct OpenIdTokenProviderConfigArguments {
         default_value = "30s"
     )]
     pub refresh_before: humantime::Duration,
+    /// Allows to use TLS in an insecure more (DANGER!)
+    #[arg(
+        id = "oidc_tls_insecure",
+        long = "oidc-tls-insecure",
+        default_value = "false"
+    )]
+    pub tls_insecure: bool,
 }
 
 impl OpenIdTokenProviderConfigArguments {
@@ -50,6 +57,7 @@ pub struct OpenIdTokenProviderConfig {
     pub client_secret: String,
     pub issuer_url: String,
     pub refresh_before: humantime::Duration,
+    pub tls_insecure: bool,
 }
 
 impl OpenIdTokenProviderConfig {
@@ -72,6 +80,7 @@ impl OpenIdTokenProviderConfig {
                     client_secret,
                     issuer_url,
                     refresh_before: arguments.refresh_before,
+                    tls_insecure: arguments.tls_insecure,
                 })
             }
             _ => None,
@@ -117,9 +126,26 @@ impl OpenIdTokenProvider {
 
     pub async fn with_config(config: OpenIdTokenProviderConfig) -> anyhow::Result<Self> {
         let issuer = Url::parse(&config.issuer_url).context("Parse issuer URL")?;
-        let client = openid::Client::discover(config.client_id, config.client_secret, None, issuer)
-            .await
-            .context("Discover OIDC client")?;
+
+        let mut client = reqwest::ClientBuilder::new();
+
+        if config.tls_insecure {
+            log::warn!("Using insecure TLS when communicating with the OIDC issuer");
+            client = client
+                .danger_accept_invalid_hostnames(true)
+                .danger_accept_invalid_certs(true);
+        }
+
+        let client = openid::Client::discover_with_client(
+            client.build()?,
+            config.client_id,
+            config.client_secret,
+            None,
+            issuer,
+        )
+        .await
+        .context("Discover OIDC client")?;
+
         Ok(Self::new(
             client,
             time::Duration::try_from(<_ as Into<std::time::Duration>>::into(
