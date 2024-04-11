655245093a43bdd79576071ead5a0948f45ab3c3
Jens Reimann <ctron@dentrassi.de>
2024-04-11 12:00:56 +0200

    chore: skip empty keys field

--- sbom/src/model/metadata.rs
+++ sbom/src/model/metadata.rs
@@ -20,6 +20,7 @@ impl From<Url> for Key {
 #[derive(Clone, Debug, PartialEq, Eq, serde::Deserialize, serde::Serialize)]
 pub struct SourceMetadata {
     pub url: Url,
+    #[serde(default, skip_serializing_if = "Vec::is_empty")]
     pub keys: Vec<Key>,
 }
 
