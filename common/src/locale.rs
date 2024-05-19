d36dbb9399566496e07ca1d11d8c4cae2dc81441
Jens Reimann <ctron@dentrassi.de>
2024-05-19 14:21:35 +0200

    chore: let's start with a very simple approach to formatting

--- common/src/locale.rs
+++ common/src/locale.rs
@@ -1,10 +1,11 @@
 use std::fmt::{Display, Formatter};
+use thousands::Separable;
 
 /// Format a number in a locale specific way
 pub struct Formatted(pub usize);
 
 impl Display for Formatted {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.0)
+        write!(f, "{}", self.0.separate_with_commas())
     }
 }
