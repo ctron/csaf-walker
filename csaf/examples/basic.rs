f836315b384db1d2812b3c3475678a688520f38c
Jens Reimann <ctron@dentrassi.de>
2024-05-19 12:50:22 +0200

    feat(csaf): add a basic example

---
+++ csaf/examples/basic.rs
@@ -0,0 +1,26 @@
+use csaf_walker::{
+    retrieve::RetrievingVisitor,
+    source::{HttpOptions, HttpSource},
+    validation::ValidationVisitor,
+    walker::Walker,
+};
+use walker_common::fetcher::{Fetcher, FetcherOptions};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let fetcher = Fetcher::new(FetcherOptions::default()).await?;
+    let source = HttpSource::new("redhat.com", fetcher, HttpOptions::default());
+
+    let validator = ValidationVisitor::new(|result| async {
+        match result {
+            Ok(doc) => println!("Document: {doc:?}"),
+            Err(err) => println!("Failed: {err}"),
+        }
+        Ok::<_, anyhow::Error>(())
+    });
+    let retriever = RetrievingVisitor::new(source.clone(), validator);
+
+    Walker::new(source).walk(retriever).await?;
+
+    Ok(())
+}
