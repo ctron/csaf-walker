54b6bd836289da683791b64cab804b7697679dc4
Jens Reimann <ctron@dentrassi.de>
2024-07-09 10:03:17 +0200

    feat: add a check for duplicate nodes

--- sbom/sbom-cli/src/cmd/report/check/mod.rs
+++ sbom/sbom-cli/src/cmd/report/check/mod.rs
@@ -3,6 +3,7 @@ mod spdx;
 use crate::cmd::report::ReportSink;
 use sbom_walker::Sbom;
 
+/// Run all SBOM checks
 pub fn all(report: &dyn ReportSink, sbom: Sbom) {
     if let Sbom::Spdx(sbom) = sbom {
         spdx::all(report, sbom)
--- sbom/sbom-cli/src/cmd/report/check/spdx.rs
+++ sbom/sbom-cli/src/cmd/report/check/spdx.rs
@@ -1,7 +1,8 @@
 use crate::cmd::report::ReportSink;
 use spdx_rs::models::SPDX;
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
+/// Run all SPDX sbom checks
 pub fn all(report: &dyn ReportSink, spdx: SPDX) {
     SpdxChecks {
         report,
@@ -18,6 +19,30 @@ struct SpdxChecks<'c> {
 impl SpdxChecks<'_> {
     pub fn all(&self) {
         self.rel_targets();
+        self.duplicate_packages();
+    }
+
+    /// check for duplicate nodes
+    fn duplicate_packages(&self) {
+        let mut ids = HashMap::with_capacity(
+            self.spdx.package_information.len() + self.spdx.file_information.len() + 1,
+        );
+
+        ids.insert(&self.spdx.document_creation_information.spdx_identifier, 1);
+
+        for package in &self.spdx.package_information {
+            *ids.entry(&package.package_spdx_identifier).or_default() += 1;
+        }
+        for file in &self.spdx.file_information {
+            *ids.entry(&file.file_spdx_identifier).or_default() += 1;
+        }
+
+        for (id, num) in ids {
+            if num > 1 {
+                self.report
+                    .error(format!("Duplicate SPDX ID '{id}', occurred {num} times."));
+            }
+        }
     }
 
     /// check if all relationships have valid targets
