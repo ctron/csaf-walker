42f9781868e2933afb77ca9da43c496c0e9cf2cc
Jens Reimann <ctron@dentrassi.de>
2024-07-09 10:34:38 +0200

    chore: make clippy happy

--- sbom/src/report/check/mod.rs
+++ sbom/src/report/check/mod.rs
@@ -5,7 +5,7 @@ use super::ReportSink;
 use crate::Sbom;
 
 /// Run all SBOM checks
-pub fn all(report: &dyn ReportSink, sbom: Sbom) {
+pub fn all(#[allow(unused)] report: &dyn ReportSink, #[allow(unused)] sbom: Sbom) {
     #[cfg(feature = "spdx-rs")]
     if let Sbom::Spdx(sbom) = sbom {
         spdx::all(report, sbom)
