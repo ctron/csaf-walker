490ea4415086b2ba533ba45c54f1b3742e490170
Jens Reimann <ctron@dentrassi.de>
2024-10-29 11:43:01 +0100

    docs: provide some more documentation

--- common/src/retrieve/error.rs
+++ common/src/retrieve/error.rs
@@ -2,12 +2,14 @@ use crate::{source::Source, utils::url::Urlify};
 use std::fmt::Debug;
 use url::Url;
 
+/// An error from a retrieval visitor.
 #[derive(Clone, Debug, thiserror::Error)]
 pub enum RetrievalError<D, S>
 where
     D: Urlify,
     S: Source,
 {
+    /// Failed to fetch the discovered document from the source.
     #[error("source error: {err}")]
     Source { err: S::Error, discovered: D },
 }
